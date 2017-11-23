777c2994029a92c7cec36be3e35b58ba8772b3bf
David Zhao <david.zhao14@gmail.com>
2017-11-23 12:41:46 -0500

    welp gotta allocate mem from the heap for large stuff

---
+++ practice/data_structures/arrays/array-manipulation.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int n, m, a, b;
+	cin >> n >> m;
+	long k;
+	long* counts = new long[n];
+	for (int i=0; i<m; i++) {
+		cin >> a >> b >> k;
+		a -= 1; b -= 1;
+		counts[a] += k;
+		if (b < n-1) {
+			counts[b+1] -= k;
+		}
+	}
+	long ma = 0;
+	long v = 0;
+	for (int i=0; i < n; i++) {
+		v += counts[i];
+		ma = max(ma, v);
+	}
+	cout << ma;
+
+	return 0;
+}
